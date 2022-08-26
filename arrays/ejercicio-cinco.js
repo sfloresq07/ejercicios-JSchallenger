885797af539d2b4f77d634d1dee844e122ccc364
Sandra Flores <sfloresq07@gmail.com>
2022-08-26 19:43:43 -0400

    Ejercicio del uno al seis de arrays

---
+++ arrays/ejercicio-cinco.js
@@ -0,0 +1,6 @@
+// Write a function that takes an array (a) and a number (n) as arguments
+// It should return the last n elements of a
+function myFunction(a, n) {
+  return a.splice(-n);
+}
+console.log(myFunction([1, 2, 3, 4, 5, 6, 7, 8], 3));
